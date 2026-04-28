037f6ea7fe7e64e5413cc1811d777b5852c41e3c
LeoSayYeah <Leo_mcdonnell@hotmail.com>
2026-04-28 16:34:47 +0100

    Update vite.config.js

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    allowedHosts: ['lcm-frontend-lcm-1.onrender.com']
+    host: '0.0.0.0',
+    allowedHosts: true
   }
 })
